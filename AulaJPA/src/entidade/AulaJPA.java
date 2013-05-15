b2be8fb238e9fcdf4e5e08e80e5fd26172ba7268
veronica <veronica@192.168.230.159>
2013-05-15 11:06:13 -0300

    salvar

---
+++ AulaJPA/src/entidade/AulaJPA.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidade;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author veronica
+ */
+@Entity
+@Table (name="Vendas")
+public class AulaJPA implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name="nome",length=255)
+    private String nome;
+
+   @Temporal(TemporalType.TIMESTAMP)
+   @Column(name="data")
+ 
+    private Date data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof AulaJPA)) {
+            return false;
+        }
+        AulaJPA other = (AulaJPA) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidade.AulaJPA[ id=" + id + " ]";
+    }
+    
+}
